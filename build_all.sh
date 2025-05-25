85b381a920376074ff8cd99a2f0a1d1bdcfca782
Timofey Mukha <timofey.mukha@protonmail.com>
2025-05-25 14:36:39 +0200

    Update

--- build_all.sh
+++ build_all.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+apptainer build neko_base.sif neko_base.def
 apptainer build neko_cpu.sif neko_cpu.def
 apptainer build neko_cuda.sif neko_cuda.def
 apptainer build neko_hip.sif neko_hip.def
